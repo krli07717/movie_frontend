4e494e198d4b93c32cb07fdb4b1096ceabd7eeb0
kunjuili <k.r.li07717@gmail.com>
2020-12-23 22:56:21 +0800

    first commit

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "react-router-dom";
 
+const imagePrefix = `https://image.tmdb.org/t/p/w185/`;
+// poster_sizes:"w92","w154","w185","w342","w500","w780","original"
+
 function MovieCard({
   id,
   title,
@@ -10,14 +13,18 @@ function MovieCard({
   vote_average,
   vote_count,
 }) {
+  const imageUrl = `${imagePrefix}` + `${poster_path}`;
   return (
     <div>
+      {/* <Link to=""> */}
+      <div>
+        <img src={imageUrl} alt="" />
+      </div>
+      {/* </Link> */}
       <p>
         {id}
         {title}
-        {poster_path}
         {release_date}
-        {overview}
         {vote_average}
         {vote_count}
       </p>
